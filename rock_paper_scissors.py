b388030753b3062c6fc29511a8d461df468ec785
Jasmine <jasminedhaliwal29@gmail.com>
2020-02-14 15:41:47 +0000

    Update rock_paper_scissors.py

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,30 +1,33 @@
 from random import randint
 
-users_choice = input("Make your move: ")
-users_choice = users_choice.lower()
-x = ['rock','paper','scissors']
-num = randint(0,2)
-computers_choice = x[num]
-print (computers_choice)
+def rock_paper_scissors():
+    print ("Welcome!")
+    users_choice = input("Make your move: ")
+    users_choice = users_choice.lower()
+    x = ['rock','paper','scissors']
+    num = randint(0,2)
+    computers_choice = x[num]
+    print (computers_choice)
+    
+    if users_choice == computers_choice:
+        print ("It's a tie!")
+    elif users_choice == 'rock':
+        if computers_choice == 'paper':
+            print ("Computer wins!")
+        else:
+            # user input is scissors
+            print ("User wins!")
+    elif computers_choice == 'paper':
+        if users_choice == 'scissors':
+            print ("User wins!")
+        else:
+            # user input is rock
+            print ("Computer wins!")
+    elif computers_choice == 'scissors':
+            if users_choice == 'paper':
+                print ("Computer wins!")
+            else:
+                # user input is rock
+                print ("User wins!")
 
-
-if users_choice == computers_choice:
-	print ("It's a tie!")
-elif users_choice == 'rock':
-	if computers_choice == 'paper':
-		print ("Computer wins!")
-	else:
-		# user input is scissors
-		print ("User wins!")
-elif computers_choice == 'paper':
-	if users_choice == 'scissors':
-		print ("User wins!")
-	else:
-		# user input is rock
-		print ("Computer wins!")
-elif computers_choice == 'scissors':
-	if users_choice == 'paper':
-		print ("Computer wins!")
-	else:
-		# user input is rock
-		print ("User wins!") 
+rock_paper_scissors()
